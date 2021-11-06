e13de1e4aaac5e08d13bf5f9489cf7c4f3faa6b7
Apoorv Taneja <purutaneja.com@gmail.com>
2021-11-06 18:59:49 +0530

    added namespace

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
-
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'ca',
+});
 export default () => {
   return (
-    <div>
-      <h1>Hi there!</h1>
-      <hr />
-
-      
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassname}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
+
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'ma',
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassname}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
